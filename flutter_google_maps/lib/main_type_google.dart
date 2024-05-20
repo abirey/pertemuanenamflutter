1a38fd811c6f391e0af6e41d49be319cea50c5e5
Abiyan_3 <awalraya4@gmail.com>
2024-05-20 13:18:37 +0700

    setup

---
+++ flutter_google_maps/lib/main_type_google.dart
@@ -0,0 +1,14 @@
+enum Type { normal, hybrid, terrain, satellite }
+
+class MapTypeGoogle {
+  Type type;
+
+  MapTypeGoogle({required this.type});
+}
+
+List<MapTypeGoogle> googleMapTypes = [
+  MapTypeGoogle(type: Type.normal),
+  MapTypeGoogle(type: Type.hybrid),
+  MapTypeGoogle(type: Type.terrain),
+  MapTypeGoogle(type: Type.satellite),
+];
